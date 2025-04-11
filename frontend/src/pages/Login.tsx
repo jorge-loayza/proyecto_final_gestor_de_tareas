d1b909aa10c1fb94852d7358f82fd5b439eb632e
Jorge Loayza <jorgeloayzarios@gmail.com>
2025-04-10 22:00:23 -0400

    cambiar a dark theme por defecto

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -32,7 +32,7 @@ const Login = () => {
   };
 
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-100 dark:bg-gray-900">
+    <div className="flex items-center justify-center min-h-screen dark:bg-gray-900 dark text-foreground bg-background">
       <Card className="w-full max-w-md">
         <CardHeader className="flex justify-center">
           <h1 className="text-2xl font-bold text-center">Iniciar Sesión</h1>
